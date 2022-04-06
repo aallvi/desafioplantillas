9c065409c89cabd6ec48a7c97560f9dcf4ac4212
Alvaro <aleiva97@gmail.com>
2022-04-06 15:38:09 -0400

    api rest

--- server.js
+++ server.js
@@ -3,19 +3,100 @@ const fs = require('fs')
 const random = require('random')
 const app = express()
 
-app.get('/productos', (req,res) => {
-    const productos =  fs.readFileSync('productos.json', 'utf-8')
-    // const productosObj = JSON.parse(productos)
-    res.send(`${productos}`)
-} )
-app.get('/productoRandom', (req,res) => {
-    const productos =  fs.readFileSync('productos.json', 'utf-8')
-    const productosObj = JSON.parse(productos)
-    const idProductoRandom =  random.int((min = 1), (max = 3))
-    const productoaMostrar = productosObj.filter(productos => productos.id === idProductoRandom  )
+const routerProductos = express.Router()
 
-    res.send({productoaMostrar})
+app.use('/api/productos', routerProductos)
+
+routerProductos.use(express.json())
+routerProductos.use(express.urlencoded({extended:true}))
+
+app.use(express.static('public'))
+
+
+// ------------------------
+
+const productos = [{
+    "title":"Celular",
+    "precio":30000,
+    "thumbnail":"fotoCelular.jpg",
+    "id":1
+},{
+    "title":"Computador",
+    "precio":50000,
+    "thumbnail":"fotoComputador.jpg",
+    "id":2
+},{
+    "title":"Teclado",
+    "precio":10000,
+    "thumbnail":"fotoTeclado.jpg",
+    "id":3
+}
+]
+
+routerProductos.get('/', (req,res) => {
+
+    res.json(productos)
 } )
+
+
+routerProductos.post('/', (req,res) => {
+
+    productos.push(req.body)
+    productos[productos.length-1].id = productos.length
+
+    res.json(productos)
+} )
+
+
+routerProductos.put('/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    
+
+
+   let found = productos.find(item => item.id === id)
+
+   if(!found){
+
+          
+          return res.json('No existe producto con ese id')
+   }
+
+
+    productos.map(function(item){
+        if(item.id === id){
+          item.title = req.body.title;
+          res.json( productos)
+
+        } 
+        
+       
+      });
+
+
+
+} )
+
+routerProductos.delete('/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+
+    let found = productos.find(item => item.id === id)
+
+   if(!found){
+
+          
+          return res.json('No existe producto con ese id')
+   }
+     const productosFiltrados = productos.filter(item => item.id !== id )
+     
+   
+
+    res.json( productosFiltrados)
+} )
+
+
+
+
+
 
 const PORT = 8080;
 
